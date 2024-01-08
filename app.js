d715ef0d05b24b32683811f574c1440fe0bf5ebe
RyanKaleliGabriel <ryang9241@gmail.com>
2024-01-08 15:45:57 +0300

    Routing and middleware

--- app.js
+++ app.js
@@ -1,33 +1,163 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
+
 const app = express();
+app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res
-//     .status(404)
-//     .json({ message: "Hello from the server siiiiiiide", app: "Natours" });
-// });
+// 1)MIDDLEWARES
+app.use(morgan("dev"));
 
-// app.post("/", (req, res) => {
-//   res.send("You can post to this endpoint..");
-// });
+app.use((req, res, next) => {
+  console.log("Hello from the middleware🤵‍♂️");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 //Converts to a js object
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     //success 200 code    //fail if there was an error at the client      //error if there was an error in the client
     status: "success",
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
     },
   });
-});
+};
 
+//3)ROUTE HANDLERS
+const getTour = (req, res) => {
+  console.log(req.params);
+  //converting a string to a number
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  //   if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: tour,
+    },
+  });
+};
+const createTour = (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "Success",
+        data: {
+          tour: newTour,
+        },
+      }); //201 stands for created
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  //   if (id > tours.length) {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "Success",
+    data: {
+      tour: "<Updated tour here...>",
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  //204 means no content
+  res.status(204).json({
+    status: "Success",
+    data: null,
+  });
+};
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+//3)ROUTES
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route("/").get(getAllTours).post(createTour);
+tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route("/").get(getAllUsers).post(createUser);
+userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+//4 START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
