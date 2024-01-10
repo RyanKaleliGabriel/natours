c2d3c9549643cebda9dbebf8d11b81ee23230acb
RyanKaleliGabriel <ryang9241@gmail.com>
2024-01-10 13:59:35 +0300

    MVC Code Refactor, CRUD OPERATIONS

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express')
+const morgan = require('morgan')
 
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
-const app = express();
+const app = express()
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
 }
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware🤵‍♂️");
-  next();
-});
+  console.log('Hello from the middleware🤵‍♂️')
+  next()
+})
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+  req.requestTime = new Date().toISOString()
+  next()
+})
 
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
-module.exports = app;
+module.exports = app
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const tourController = require("./../controllers/tourController");
+const express = require('express')
+const tourController = require('./../controllers/tourController')
 
-const router = express.Router();
+const router = express.Router()
 
-router.param("id", tourController.checkId);
+// router.param("id", tourController.checkId);
 
 router
-  .route("/")
+  .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour)
 
 router
-  .route("/:id")
+  .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(tourController.deleteTour)
 
-module.exports = router;
+module.exports = router
