431a613b2946e0e4f69c981bcabbe9a304b26105
RyanKaleliGabriel <ryang9241@gmail.com>
2024-01-18 13:15:14 +0300

    Authentication & Authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require('../utils/catchAsync');
 const AppError = require("../utils/appError");
+const sendEmail = require("../utils/email");
+const crypto = require('crypto');
 
 
 const signToken = id => {
@@ -17,7 +19,8 @@ exports.signup = catchAsync(async (req, res, next) => {
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
-        passwordChangedAt: req.body.passwordChangedAt
+        passwordChangedAt: req.body.passwordChangedAt,
+        role: req.body.role,
     });
 
     const token = signToken(newUser._id);
@@ -69,18 +72,101 @@ exports.protect = catchAsync(async (req, res, next) => {
 
     //3) Check if users still exists 
     //The user may be deleted
-    const freshUser = await User.findById(decoded.id);
-    if(!freshUser){
-        return next(new AppError('The user belonging to this token does no longer exists', 401))
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(new AppError('The user belonging to this token does no longer exists', 401));
     }
 
     //4)CHECK IF USER changed password after the token was issued
 
-    if(freshUser.changedPasswordAfter(decoded.iat)){
-        return next(new AppError('Recently changed password! Please login again', 401))
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(new AppError('Recently changed password! Please login again', 401));
     }
 
     //Grant access to protected route
-    req.user = freshUser
+    req.user = currentUser;
     next();
+});
+
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        //Roles is an array ['admin', 'lead-guide']
+        if (!roles.includes(req.user.role)) {
+            return next(new AppError("You do not have permission to perform this action", 403));
+            //403 means forbidden
+        }
+        next();
+    };
+};
+
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+    //1)Get user based on Posted email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return next(new AppError('There is no user with email address', 404));
+    }
+
+    //2)Generate random reset token
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+
+    //3)Send it to user's email
+    //req.protocol is http or https
+    const resetURL = `${req.protocol}://${req.get('host')}/v1/users/resetPassword/${resetToken}`;
+
+    const message = `Forgot your password? Submit a PATCH request with your new password and
+     new passwordConfirm to : ${resetURL}.\nIf you didn't forget your password, please ignore this`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: "Your password reset token (valid for 10 min)",
+            message
+        });
+
+        res.status(200).json({
+            status: 'Success',
+            message: 'Token sent to email!'
+        });
+
+    } catch (err) {
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+        return next(new AppError("There was an error sending email.Try again later!"), 500);
+    }
+
+});
+exports.resetPassword = catchAsync(async (req, res, next) => {
+    //1) GET USER BASED ON THE TOKEN
+    const hashedToken = crypto
+        .createHash('sha256')
+        .update(req.params.token)
+        .digest('hex');
+
+    const user = await User.findOne({
+        passwordResetToken: hashedToken,
+        passwordResetExpires: { $gt: Date.now() }
+    });
+
+    //2)SET NEW PASSWORD IF TOKEN HAS NOT EXPIRED
+    if (!user) {
+        return next(new AppError('Token is invalid or has expired'));
+    }
+
+    //3)Update changedPasswordAt property for the user
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+
+    //4) Log the user in, send JWT
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token
+    });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require("bcryptjs");
@@ -25,6 +26,11 @@ const userSchema = new mongoose.Schema({
         minlength: 8,
         select: false
     },
+    role: {
+        type: String,
+        enum: ['user', 'guide', 'lead-guide', 'admin'],
+        default: 'user'
+    },
     passwordConfirm: {
         type: String,
         required: [true, 'Please confirm your password'],
@@ -38,7 +44,9 @@ const userSchema = new mongoose.Schema({
     },
     passwordChangedAt: {
         type: Date
-    }
+    },
+    passwordResetToken: String,
+    passwordResetExpires: Date
 });
 
 // Mongoose DOCUMENT MIDDLEWARE runs before .save() and .create() but not .insertMany
@@ -56,6 +64,15 @@ userSchema.pre('save', async function (next) {
     next();
 });
 
+userSchema.pre('save', function (next) {
+    //check if a new password was created which is modification of the password field or if the password was modified
+    if (!this.isModified('password') || this.isNew) return next();
+
+    //To ensure the token is created after the password has been changed
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
 // This is an insance method it it available in th document created 
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword);
@@ -63,13 +80,26 @@ userSchema.methods.correctPassword = async function (candidatePassword, userPass
 
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     if (this.passwordChangedAt) {
-        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10)
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
         console.log(changedTimestamp, JWTTimestamp);
-        return JWTTimestamp < changedTimestamp 
+        return JWTTimestamp < changedTimestamp;
     }
 
     //False means not changed
     return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    console.log({ resetToken }, this.passwordResetToken);
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+    return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
