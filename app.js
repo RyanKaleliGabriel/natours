64819ee998617c65609004107b3ecdf89c56c106
RyanKaleliGabriel <ryang9241@gmail.com>
2024-01-23 15:03:12 +0300

    Pug Templating

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -11,10 +12,18 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoute')
+const viewRouter = require('./routes/viewRoutes')
 
 const app = express();
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
 // 1) GLOBAL MIDDLEWARES
+
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Setting security https headers
 app.use(helmet());
@@ -59,9 +68,6 @@ app.use(hpp({
     'price']
 }));
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -69,6 +75,8 @@ app.use((req, res, next) => {
   next();
 });
 
+// Routes
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter)
