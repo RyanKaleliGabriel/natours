cc080c7377d8b1ac6c81eff665d51dbfaf0c2f2d
RyanKaleliGabriel <ryang9241@gmail.com>
2024-01-30 12:56:24 +0300

    Bookings Model

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -69791,7 +69791,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57164" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61772" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,7 +4,20 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.protect)
+
 // will not follow the rest principal
-router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession)
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession)
+
+router.use(authController.restrictTo('admin', 'lead-guide'))
+
+router.route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking)
+
+router.route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking)
 
 module.exports = router;
