aef7d3dac4c85adfc6852aa6f9f65ce3d67b56e5
RyanKaleliGabriel <ryang9241@gmail.com>
2024-01-30 18:10:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const app = require('./app');
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useNewUrlParser: true
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  // useUnifiedTopology: true
 }).then(() => console.log('Db connection successful'));
 
 const port = process.env.PORT || 3000;
