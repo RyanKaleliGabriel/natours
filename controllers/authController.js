4bb1231262c8188c1032d34e656c231d3f8cb1af
RyanKaleliGabriel <ryang9241@gmail.com>
2024-01-30 18:54:58 +0300

    Testing for secure https

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,22 +12,21 @@ const signToken = id => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, res, req) => {
   const token = signToken(user._id);
 
   // secure stands for only https
   // httpOnly the browser cannot modify the cookie. The broswer on recieves the cookie, stores it and sends it with other requests
-  const cookieOptions = {
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-    // this means we cannot manipulate/change it/ destroy the cookie in the browser in any way or c
-    httpOnly: true
-  };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
   // removes password from the output
   user.password = undefined;
 
-  res.cookie('jwt', token, cookieOptions);
+  res.cookie('jwt', token, {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    // this means we cannot manipulate/change it/ destroy the cookie in the browser in any way or c
+    httpOnly: true,
+    secure: (req.secure || req.header('x-forwarded-proto') === 'https')
+  });
 
   res.status(statusCode).json({
     status: 'success',
@@ -37,6 +36,7 @@ const createSendToken = (user, statusCode, res) => {
     }
   });
 };
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -51,7 +51,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   // console.log(url);
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, res, req);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -68,7 +68,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password', 401)); // 401 means unauthorized
   }
   // 3) if everything is ok, send token to client
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, res, req);
 });
 
 exports.logout = (req, res) => {
@@ -209,7 +209,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   await user.save();
 
   // 4) Log the user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, res, req);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -229,5 +229,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   // User.findByIdanUpdate will not work as intended because of our schema and document middlewares
 
   // 4)Log user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, res, req);
 });
